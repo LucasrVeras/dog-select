b0fd32ebc3c6888402da8854df5f6f54578ece4f
LucasrVeras <lucas.veras01@gmail.com>
2022-03-13 17:27:32 -0300

    Project started

---
+++ backend/src/main/java/br/com/lucas/dogselect/DogSelectApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucas.dogselect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogSelectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogSelectApplication.class, args);
+	}
+
+}
